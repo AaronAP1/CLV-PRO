67566b8192c7f46c83bb2759d7c0086c13bdd78a
Mijael Aliaga <mijael.aliaga@upeu.edu.pe>
2023-12-07 15:42:32 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-
+  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
